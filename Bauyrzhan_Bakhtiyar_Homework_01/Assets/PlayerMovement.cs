61b25cece60dd0a0d88bdf61ede702415065ab6b
KitsuneTheBest <baxa.blackburn@gmail.com>
2022-02-18 22:38:19 +0300

    This should work

--- Bauyrzhan_Bakhtiyar_Homework_01/Assets/PlayerMovement.cs
+++ Bauyrzhan_Bakhtiyar_Homework_01/Assets/PlayerMovement.cs
@@ -3,9 +3,9 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float Speed = 1f;
+    public float JumpVelocity = 5f;
 
     private int _coinCounter;
-
     private int _directionIndex;
     private Rigidbody _rigidbody;
 
@@ -18,6 +18,15 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         if (Input.GetMouseButtonDown(0)) _directionIndex = _directionIndex == 0 ? 1 : 0;
+        
+        var altitude = transform.position;
+        
+        if (Input.GetButtonDown("Jump") && (altitude.y >= 0.998 && altitude.y <= 1.002))
+        {
+            var velocity = _rigidbody.velocity;
+            velocity.y = JumpVelocity;
+            _rigidbody.velocity = velocity;
+        }
     }
 
     private void FixedUpdate()
